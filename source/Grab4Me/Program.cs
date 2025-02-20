24a725b015c74edfef51c9023a9304cb3c0c6cf7
boringthings <boringsoft-gh@mailbox.org>
2025-02-20 18:49:59 +0000

    Added basic webapi

---
+++ source/Grab4Me/Program.cs
@@ -0,0 +1,20 @@
+namespace Grab4Me;
+
+using Microsoft.AspNetCore.Builder;
+
+public class Program
+{
+    public static int Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!"); //TODO: list all available resources
+        app.MapGet("/{*path}", (string path) => $"Hello {path}!"); // TODO get the resource
+
+
+        app.Run("http://*:5000");
+
+        return 0;
+    }
+}
